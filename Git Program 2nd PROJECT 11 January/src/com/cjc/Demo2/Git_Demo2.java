a27641367e4cad88049c5533ca4ef5649208b0d5
rutur <rutur@RUTURAJ>
2024-01-11 12:36:29 +0530

    By Ruturaj

---
+++ Git Program 2nd PROJECT 11 January/src/com/cjc/Demo2/Git_Demo2.java
@@ -0,0 +1,26 @@
+package com.cjc.Demo2;
+
+public class Git_Demo2 
+{
+	public void m5() 
+    {
+ 	   
+ 	   System.out.println("method m5");
+    }
+	
+	public void m6() 
+    {
+ 	   
+ 	  System.out.println("Method m6"); 
+    }
+	public static void main(String[] args) 
+	{
+		System.out.println("Main check");
+		Git_Demo2 g = new Git_Demo2 ();
+		g.m5();
+		g.m6();
+
+
+	}
+
+}
